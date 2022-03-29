7e043f1b7f90dc75a1664cd061daccee4c052d61
Aniruddha <aniruddha1712@gmail.com>
2022-03-29 15:46:03 +0530

    [Aniruddha]Added: Daily employee wage calculation program

--- EmployeeWageComputation/EmpWageComputation.cs
+++ EmployeeWageComputation/EmpWageComputation.cs
@@ -12,16 +12,23 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             const int IS_FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if(empCheck==IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee Absent");
+                empHrs = 0;
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily employee wage is:{0}",empWage);
             Console.ReadLine();
         } 
     }   
