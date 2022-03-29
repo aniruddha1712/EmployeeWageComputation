872d88ba2b712d9d519f1cf3a1d3ef79d5d9fb49
Aniruddha <aniruddha1712@gmail.com>
2022-03-29 16:10:36 +0530

    [Aniruddha]Added: Using switch case program to calculate daily wage

--- EmployeeWageComputation/EmpWageComputation.cs
+++ EmployeeWageComputation/EmpWageComputation.cs
@@ -8,29 +8,29 @@ namespace EmployeeWageComputation
 {
     class EmpWageComputation
     {
+            public const int IS_FULL_TIME = 1;
+            public const int IS_PART_TIME = 2;
+            public const int EMP_RATE_PER_HR = 20;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if(empCheck == IS_FULL_TIME)
+            switch(empCheck)
             {
-                empHrs = 8;
-            }
-            else if(empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Daily employee wage is:{0}",empWage);
