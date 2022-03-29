294b66c80ac7f3b243c85b38b596c65ba0af4fdf
Aniruddha <aniruddha1712@gmail.com>
2022-03-29 16:27:08 +0530

    [Aniruddha]Added: Max number of working days

--- EmployeeWageComputation/EmpWageComputation.cs
+++ EmployeeWageComputation/EmpWageComputation.cs
@@ -11,29 +11,34 @@ namespace EmployeeWageComputation
             public const int IS_FULL_TIME = 1;
             public const int IS_PART_TIME = 2;
             public const int EMP_RATE_PER_HR = 20;
+            public const int NUM_OF_WORKING_DAYS = 20;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0 ,  empWage = 0, totalEmpWage=0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HR * empHrs;
+                Console.WriteLine("Employee wage for day{0} is :{1}",day, empWage);
+                totalEmpWage += empWage;
             }
-            empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Daily employee wage is:{0}",empWage);
+            Console.WriteLine("Total employee wage: {0}", totalEmpWage);
             Console.ReadLine();
         } 
     }   
